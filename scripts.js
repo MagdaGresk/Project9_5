5001752aa9270a7c5deb06bd359ef7ef1c0c7663
MagdaGresk <36829944+MagdaGresk@users.noreply.github.com>
2018-03-19 14:48:11 -0400

    Correct code in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,8 @@
-var withButtonClass = document.getElementsByClassName('button');
-console.log(withButtonClass);
-var buttonLength = document.getElementsByClassName('button').length;
-console.log(buttonLength);
+var withButtonClass = document.getElementsByClassName('button')
+console.log(withButtonClass)
+var buttonLength = withButtonClass.length;
+console.log(buttonLength)
 
-
-for (var n = 0; n <= buttonLength; n++) {
-	var innerText = '';
-	innerText = withButtonClass[n];
-	console.log(innerText);
+for (var n = 0; n < buttonLength; n++) {
+	console.log(withButtonClass[n].innerText)
 }
